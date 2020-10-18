e8d9662c9a896d084750f2bbabf49c06fac9994f
Iraj Azarvand <vazhenegar@hotmail.com>
2020-10-18 13:36:29 +0330

    add admin panel dashboard with css and js

---
+++ resources/views/PageElements/Dashboard/Css.blade.php
@@ -0,0 +1,14 @@
+<!-- Bootstrap 3.3.7 -->
+<link rel="stylesheet" href="{{asset('Panel/dist/css/bootstrap-theme.css')}}">
+<!-- Bootstrap rtl -->
+<link rel="stylesheet" href="{{asset('Panel/dist/css/rtl.css')}}">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{asset('Panel/bower_components/font-awesome/css/font-awesome.min.css')}}">
+<!-- Ionicons -->
+<link rel="stylesheet" href="{{asset('Panel/bower_components/Ionicons/css/ionicons.min.css')}}">
+<!-- jvectormap -->
+<link rel="stylesheet" href="{{asset('Panel/bower_components/jvectormap/jquery-jvectormap.css')}}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{asset('Panel/dist/css/AdminLTE.css')}}">
+
+<link rel="stylesheet" href="{{asset('Panel/dist/css/skins/_all-skins.min.css')}}">
