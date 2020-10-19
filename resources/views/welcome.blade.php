39bc564b650a6168d19344ca3367de56f3e6562b
iraj <vazhenegar@hotmail.com>
2020-10-19 22:42:30 +0330

    removed old template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,2 @@
-@extends('PageElements.Main.Shared.MasterLayout')
-
-@section('slier')
-    @include('PageElements.Main.Index.Slider')
-@endsection
-
-@section('content')
-    @include('PageElements.Main.Index.AboutUs')
-
-    @include('PageElements.Main.Index.Products')
-
-    @include('PageElements.Main.Index.OurNews')
-
-    @include('PageElements.Main.Index.ContactUs')
-
-@endsection
+<?php
+    echo 'home';
