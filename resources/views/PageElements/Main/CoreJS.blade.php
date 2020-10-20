5b522c71dd21994bb4b0bf82f3f7d195495805ab
Iraj Azarvand <vazhenegar@hotmail.com>
2020-10-20 10:02:28 +0330

    replace new template and set fonts to vazir.

---
+++ resources/views/PageElements/Main/CoreJS.blade.php
@@ -0,0 +1,20 @@
+<script src="{{asset('Main/js/jquery-1.10.2.min.js')}}"></script>
+<script src="{{asset('Main/twitter-bootstrap/js/bootstrap.min.js')}}" type="text/javascript"></script>
+<!--Pace Page Loader -->
+<script src="{{asset('Main/js/pace-0.5.1/pace.min.js')}}"></script>
+<!--FlexSlider -->
+<script src="{{asset('Main/js/woothemes-FlexSlider-06b12f8/jquery.flexslider-min.js')}}"></script>
+<!--Isotope Plugin -->
+<script src="{{asset('Main/js/isotope/jquery.isotope.min.js')}}" type="text/javascript"></script>
+<!--To-Top Button Plugin -->
+<script type="text/javascript" src="{{asset('Main/js/jquery.ui.totop.js')}}"></script>
+<!--Easing animations Plugin -->
+<script type="text/javascript" src="{{asset('Main/js/easing.js')}}"></script>
+<!--WOW Reveal on scroll Plugin -->
+<script type="text/javascript" src="{{asset('Main/js/wow.min.js')}}"></script>
+<!--Simple Text Rotator -->
+<script type="text/javascript" src="{{asset('Main/js/jquery.simple-text-rotator.js')}}"></script>
+<!--The Theme Required Js -->
+<script type="text/javascript" src="{{asset('Main/js/cleanstart_theme.js')}}"></script>
+<!--To collapse the menu -->
+<script type="text/javascript" src="{{asset('Main/js/collapser.js')}}"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,4 @@
-<?php
-    echo 'home';
+@extends('PageElements.Main.MasterLayout')
+@section('content')
+    <p>this is a test</p>
+@endsection
