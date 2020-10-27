1eb677e406d5eb638fb1198ec58c4f0c8ee8d6be
Iraj Azarvand <vazhenegar@hotmail.com>
2020-10-27 15:11:03 +0330

    remove old css links from css blade file

--- resources/views/PageElements/Dashboard/Shared/Css.blade.php
+++ resources/views/PageElements/Dashboard/Shared/Css.blade.php
@@ -1,36 +1,3 @@
-{{--
-<!-- Bootstrap 3.3.7 -->
-<link rel="stylesheet" href="{{ asset('Panel/dist/css/bootstrap-theme.css') }}">
-<!-- Bootstrap rtl -->
-<link rel="stylesheet" href="{{ asset('Panel/dist/css/rtl.css') }}">
-<!-- Font Awesome -->
-<link rel="stylesheet" href="{{ asset('Panel/bower_components/font-awesome/css/font-awesome.min.css') }}">
-<!-- Ionicons -->
-<link rel="stylesheet" href="{{ asset('Panel/bower_components/Ionicons/css/ionicons.min.css') }}">
-<!-- jvectormap -->
-<link rel="stylesheet" href="{{ asset('Panel/bower_components/jvectormap/jquery-jvectormap.css') }}">
-<!-- Theme style -->
-<link rel="stylesheet" href="{{ asset('Panel/dist/css/AdminLTE.css') }}">
-
-<link rel="stylesheet" href="{{ asset('Panel/dist/css/skins/_all-skins.min.css') }}">
-
-<link rel="stylesheet" href="{{ asset('fonts/vazir.css') }}"> --}}
-
-
-{{-- Old css End --}}
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <!-- Font Awesome -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
