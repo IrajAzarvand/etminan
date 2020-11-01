2817f703c688bd5a41750d9cbb0f00cfbd5536e8
Iraj Azarvand <vazhenegar@hotmail.com>
2020-11-01 10:48:14 +0330

    add code to dashboard layout to show uploaded file name in file upload box

--- resources/views/PageElements/Dashboard/Shared/Js.blade.php
+++ resources/views/PageElements/Dashboard/Shared/Js.blade.php
@@ -1,4 +1,3 @@
-
 <!-- jQuery -->
 <script src="{{ asset('Panel/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
@@ -9,3 +8,13 @@
 <script src="{{ asset('Panel/dist/js/adminlte.min.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="{{ asset('Panel/dist/js/demo.js') }}"></script>
+
+<!-- get input file name in setting slider page and show in input box  -->
+<script>
+    $('#sliderImage').on('change',function(){
+        //get the file name
+        let filename = $(this).val().split("\\").pop();
+        //replace the "Choose a file" label
+        $(this).next('.custom-file-label').html(filename);
+    })
+</script>
