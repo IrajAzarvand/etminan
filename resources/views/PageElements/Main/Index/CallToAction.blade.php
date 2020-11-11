c7785351f91aba524da2d3aca99c2f4a5a81a919
Iraj Azarvand <vazhenegar@hotmail.com>
2020-11-12 00:03:40 +0330

    working on footer padding

--- resources/views/PageElements/Main/Index/CallToAction.blade.php
+++ resources/views/PageElements/Main/Index/CallToAction.blade.php
@@ -1,11 +1,11 @@
 <section class="call_to_action dark_section">
     <div class="container">
         <div class="mini-carousel">
+            <div><img src="{{asset('Main/images/about_us.jpg')}}" alt=""></div>
             <div><img src="{{asset('Main/images/2a.jpg')}}" alt=""></div>
-            <div><img src="{{asset('Main/images/2a.jpg')}}" alt=""></div>
-            <div><img src="{{asset('Main/images/2a.jpg')}}" alt=""></div>
-            <div><img src="{{asset('Main/images/2a.jpg')}}" alt=""></div>
-            <div><img src="{{asset('Main/images/2a.jpg')}}" alt=""></div>
+            <div><img src="{{asset('Main/images/4as.jpg')}}" alt=""></div>
+            <div><img src="{{asset('Main/images/1a.jpg')}}" alt=""></div>
+            <div><img src="{{asset('Main/images/3as.jpg')}}" alt=""></div>
             <div><img src="{{asset('Main/images/2a.jpg')}}" alt=""></div>
         </div>
     </div>
--- resources/views/PageElements/Main/Index/TwitterFeed.blade.php
+++ resources/views/PageElements/Main/Index/TwitterFeed.blade.php
@@ -1,8 +1,7 @@
 <section class="twitter_feed_wrapper skincolored_section">
     <div class="container">
         <div class="row">
-            <div class="twitter_feed_icon wow fadeInDown"><a href="https://twitter.com/PlethoraThemes"><i
-                            class="fa fa-twitter"></i></a></div>
+            <div class="twitter_feed_icon wow fadeInDown"><a href="https://twitter.com/PlethoraThemes"><i class="fa fa-star-o"></i></a></div>
             <div id="twitter_flexslider" class="flexslider">
                 <ul class="slides">
                     <li class="item">
