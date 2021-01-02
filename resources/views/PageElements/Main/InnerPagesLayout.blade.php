045de65cbe69a50c11a3f3d09c197986f7ef9190
Iraj Azarvand <vazhenegar@hotmail.com>
2021-01-02 14:49:01 +0330

    create separate layout file for other pages of website

---
+++ resources/views/PageElements/Main/InnerPagesLayout.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>{{config('app.name')}} - @yield('PageTitle')</title>
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="HandheldFriendly" content="true" />
+    <meta name="apple-touch-fullscreen" content="yes" />
+
+@if (app()->getLocale()=='fa' ||app()->getLocale()=='ar')
+    @include('PageElements.Main.Shared.RTLCoreCSS')
+@else
+    @include('PageElements.Main.Shared.LTRCoreCSS')
+@endif
+
+<!-- Fav and touch icons -->
+    <link rel="shortcut icon" href="{{asset('favicon.ico')}}">
+</head>
+<body class="sticky_header">
+<div class="overflow_wrapper">
+    @include('PageElements.Main.Shared.Header')
+
+    <div class="full_page_photo no_photo">
+        <div class="hgroup">
+            <div class="hgroup_title animated bounceInUp skincolored">
+                <div class="container">
+                    <h1 class="">{{$SectionTitle}}</h1>
+                </div>
+            </div>
+            <div class="hgroup_subtitle animated bounceInUp ">
+                <div class="container">
+                    <p>این نمونه کارها می توانند تبلیغ خوبی برای ما باشند!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="main">
+
+        @section('content')
+        @show
+
+        @include('PageElements.Main.Shared.Footer')
+
+    </div>
+    @include('PageElements.Main.Shared.CoreJS')
+
+
+</div>
+</body>
+</html>
--- resources/views/PageElements/Main/Product/AllProducts.blade.php
+++ resources/views/PageElements/Main/Product/AllProducts.blade.php
@@ -1,51 +1,36 @@
-@extends('PageElements.Main.MasterLayout')
+@extends('PageElements.Main.InnerPagesLayout')
 @section('PageTitle', 'محصولات')
 @section('content')
 
-    <section class="portfolio_teasers_wrapper">
-        <div class="container triangles-of-section">
-            <div class="triangle-up-left"></div>
-            <div class="square-left"></div>
-            <div class="triangle-up-right"></div>
-            <div class="square-right"></div>
-        </div>
-        <div class="container">
-            <h2 class="section_header fancy centered">{{ $SectionTitle }}</h2>
+        <section class="portfolio_strict">
+            <div class="container">
+                <ul class="portfolio_filters">
+                    <li><a href="#" data-filter="*">همه</a></li>
+                    {{--                    <li><a href="#" data-filter=".cat_artists">هنری</a></li>--}}
+                    @foreach($CategoriesList as $cat)
+                        <li><a href="#" data-filter=".{{$cat['name']}}">{{$cat['name']}}</a></li>
 
-            <section class="portfolio_strict">
-                <div class="container">
-                    <ul class="portfolio_filters">
-                        <li><a href="#" data-filter="*">همه</a></li>
-                        {{--                    <li><a href="#" data-filter=".cat_artists">هنری</a></li>--}}
-                        @foreach($CategoriesList as $cat)
-                            <li><a href="#" data-filter=".{{$cat['name']}}">{{$cat['name']}}</a></li>
+                    @endforeach
+                </ul>
+                <div class="row isotope_portfolio_container">
+                    @foreach($PList as$item)
+                        <div class="{{$item['cat']}} col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                            <div class="portfolio_item">
+                                <a href="portfolio_item.html">
+                                    <figure
+                                        style="background-image:url({{$item['image']}})">
+                                        <figcaption>
+                                            <div class="portfolio_description">
+                                                <h3>{{$item['name']}}</h3>
 
-                        @endforeach
-                    </ul>
-                    <div class="row isotope_portfolio_container">
-                        @foreach($PList as$item)
-                            <div class="{{$item['cat']}} col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                                <div class="portfolio_item">
-                                    <a href="portfolio_item.html">
-                                        <figure
-                                            style="background-image:url({{$item['image']}})">
-                                            <figcaption>
-                                                <div class="portfolio_description">
-                                                    <h3>{{$item['name']}}</h3>
-
-                                                </div>
-                                            </figcaption>
-                                        </figure>
-                                    </a>
-                                </div>
+                                            </div>
+                                        </figcaption>
+                                    </figure>
+                                </a>
                             </div>
-                        @endforeach
-
-
-                    </div>
+                        </div>
+                    @endforeach
                 </div>
-            </section>
-        </div>
-    </section>
-
+            </div>
+        </section>
 @endsection
