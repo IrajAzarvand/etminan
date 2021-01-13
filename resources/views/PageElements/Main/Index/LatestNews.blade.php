88abca4870a6b5ccb947630d745687f337c46c5c
Iraj Azarvand <vazhenegar@hotmail.com>
2021-01-13 14:43:03 +0330

    make some changes to index page

--- resources/views/PageElements/Main/Index/LatestNews.blade.php
+++ resources/views/PageElements/Main/Index/LatestNews.blade.php
@@ -1,18 +1,23 @@
-<section class="twitter_feed_wrapper skincolored_section" style="background-image:url({{ asset('storage/Main/Shared/GPlay.svg') }})">
-    <div class="container">
-        <div class="row">
-            <div class="twitter_feed_icon wow fadeInDown"><a href="#"><i class="fa fa-star-o"></i></a></div>
-            <div id="twitter_flexslider" class="flexslider">
-                <ul class="slides">
-                    @foreach ($LatestNewsTitle as $key=>$news)
-                    <li class="item">
-                        <blockquote>
-                            <p> {{$news}}</p>
-                        </blockquote>
-                    </li>
-                    @endforeach
-                </ul>
+<section class="twitter_feed_wrapper skincolored_section parallax call_to_action"
+         style="background-image:url({{ asset('storage/Main/Shared/overlay.jpg') }})">
+    <div class="overlay">
+        <div class="container">
+            <div class="row">
+                <div class="twitter_feed_icon wow fadeInDown"><a href="#"><i class="fa fa-star-o"></i></a></div>
+                <div id="twitter_flexslider" class="flexslider">
+                    <ul class="slides">
+                        @foreach ($LatestNewsTitle as $key=>$news)
+                            <li class="item">
+                                <blockquote>
+                                    <p> {{$news}}</p>
+                                </blockquote>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
 </section>
+
+
--- resources/views/PageElements/Main/Shared/Footer.blade.php
+++ resources/views/PageElements/Main/Shared/Footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <section class="footer_teasers_wrapper dark_section">
         <div class="container">
-            <div class="row">
+            <div class="row col-sm-9 col-md-9">
 
                 <div class="footer_teaser pl_about_us_widget col-sm-9 col-md-9">
                     {{-- section title --}}
@@ -10,14 +10,14 @@
                     {{-- address --}}
                     <p>{{$SharedContents[1]}}</p>
                 </div>
-                <div class="footer_teaser pl_about_us_widget col-sm-9 col-md-9">
+                <div class="footer_teaser pl_about_us_widget">
                     <p><i class="fa fa-envelope"></i> info@hajabdollah.com</p>
                 </div>
 
-                <div class="footer_teaser pl_about_us_widget col-sm-9 col-md-9">
+                <div class="footer_teaser pl_about_us_widget">
                     <p><i class="fa fa-phone"></i> 041-34328221 | 041-34328223</p>
                 </div>
-                <div class="footer_teaser pl_about_us_widget col-sm-9 col-md-9">
+                <div class="footer_teaser pl_about_us_widget">
                     <div class="footer_social">
                         <div class="social_wrapper">
                             <a href="#"><i class="fa fa-instagram"></i></a>
@@ -25,8 +25,14 @@
                         </div>
                     </div>
                 </div>
-
             </div>
+
+            <div class="row col-sm-3 col-md-3">
+                <div class="footer_teaser pl_about_us_widget">
+                    <img src="{{asset('storage/Main/Shared/footerimage.png')}}" alt="">
+                </div>
+            </div>
+
         </div>
     </section>
     <div class="copyright">
